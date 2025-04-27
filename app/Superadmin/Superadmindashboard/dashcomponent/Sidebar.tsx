206829076c1bbf383f91c1e4572fdf27f7b40385
Pawanpathariya <pawanpathariys@gmail.com>
2025-04-27 16:24:32 +0530

    m

--- app/Superadmin/Superadmindashboard/dashcomponent/Sidebar.tsx
+++ app/Superadmin/Superadmindashboard/dashcomponent/Sidebar.tsx
@@ -1,16 +1,12 @@
 'use client'
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { FaHome,FaUser, FaBars } from 'react-icons/fa';
+import { FaHome, FaUser, FaBars } from 'react-icons/fa';
 import { BsDisplay, BsShop, BsFillCartFill } from 'react-icons/bs';
 import { MdOutlineRequestPage } from 'react-icons/md';
 import { GrUserAdmin } from "react-icons/gr";
-import { CiSearch } from "react-icons/ci";
-interface SidebarProps {
-  children: React.ReactNode;
-}
 
-const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
   const toggleSidebar = () => {
@@ -26,13 +22,11 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
         <FaBars />
       </button>
       <div
-        className={`absolute top-0 left-0 h-screen w-64 bg-[#212529] p-4 mt-18 transition-transform transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        }`}
+        className={`absolute top-0 left-0 h-screen w-64 bg-[#212529] p-4 mt-18 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
         style={{ marginRight: isOpen ? 0 : '64px' }}
       >
         <div className="flex bg-[#212529] flex-col">
-          <ul className="mt-4 flex flex-col gap-5 justify-center ">
+          <ul className="mt-4 flex flex-col gap-5 justify-center">
             <li>
               <Link href="/Superadmin/Superadmindashboard" className="text-white hover:underline text-lg font-semibold">
                 <FaHome className="inline-block mr-2" /> Home
@@ -67,7 +61,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </ul>
         </div>
       </div>
-      {isOpen ? <div className="ml-64">{children}</div> : null}
+      {isOpen ? <div className="ml-64">{/* Content inside sidebar when open */}</div> : null}
     </>
   );
 };
--- app/Superadmin/Superadmindashboard/layout.tsx
+++ app/Superadmin/Superadmindashboard/layout.tsx
@@ -1,13 +1,18 @@
+'use client'
 import DashboardTopbar from './dashcomponent/DashboardTopbar';
 import Sidebar from './dashcomponent/Sidebar';
 
-const Layout: React.FC = ({ children }: { children}) => (
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
   <>
     <div className="bg-white min-h-screen fixed">
       <DashboardTopbar />
       <div className="flex text-black">
         <div className="sticky -mt-2">
-          <Sidebar />
+          <Sidebar /> 
         </div>
         <div className="text-black w-full mt-20 p-2">{children}</div>
       </div>
@@ -16,4 +21,3 @@ const Layout: React.FC = ({ children }: { children}) => (
 );
 
 export default Layout;
-
